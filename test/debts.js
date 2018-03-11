b5146ce86a785f2c332952d31969644ccc9d9eb8
Hunter-Nomad <rqj.polinovsky@gmail.com>
2018-03-11 14:35:44 +0200

    Тести по умолчанию

--- test/debts.js
+++ test/debts.js
@@ -30,9 +30,9 @@ contract('Debts', function(accounts) {
     const borrower = accounts[3];
     const value = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
     return Promise.resolve()
-    .then(() => debts.borrow(value, {from: borrower}))
-    .then(() => asserts.throws(debts.borrow(1, {from: borrower})));
-  });
+      .then(() => debts.borrow(value, {from: borrower}))
+      .then(() => asserts.throws(debts.borrow(1, {from: borrower})));
+    });
 
   it('should emit Borrowed event on borrow', () => {
     const borrower = accounts[3];
@@ -47,13 +47,67 @@ contract('Debts', function(accounts) {
     });
   });
 
-  it('should allow to borrow');
+  it('should allow to borrow', () => {
+        const borrower = accounts[2];
+        const value = 1000;
+        return Promise.resolve()
+            .then(() => debts.borrow(value, {from: borrower}))
+            .then(result => {
+                // console.log(result.logs[0].args);
+                assert.equal(result.logs[0].args.value, value); 
+                debts.debts(borrower).then(res => {
+                        assert.equal(res.toNumber(), value)
+                    }
+                );
+            })
+    });
 
-  it('should emit Repayed event on repay');
+  it('should emit Repayed event on repay', () => {
+        const borrower = accounts[3];
+        const value = 1000;
+        return Promise.resolve()
+            .then(() => debts.borrow(value, {from: borrower}))
+            .then(() => debts.repay(borrower, value, {from: OWNER}))
+            .then(result => {
+              // console.log(result);
+                assert.equal(result.logs[0].event, 'Repayed');
+                assert.equal(result.logs[0].args.by, borrower);
+                assert.equal(result.logs[0].args.value.valueOf(), value);
+            });
+    });
 
-  it('should not allow owner to borrow');
+  it('should not allow owner to borrow', () => {
+        // const borrower = accounts[3];
+        const value = 1000;
+        return Promise.resolve()
+        // .then(() => debts.borrow(value, {from: borrower}))
+        .then(() => debts.borrow(value, {from: OWNER}))
+        .then(result => {
+              // console.log(result);                            
+              // assert.equal(result.logs.length, 1)              
+              assert.equal(result.logs.length, 0)
+            });            
+  });
 
-  it('should not allow not owner to repay');
+  it('should not allow not owner to repay', () => {
+        const borrower = accounts[3];
+        const alien = accounts[2];
+        const value = 1000;
+        return Promise.resolve()
+            .then(() => debts.borrow(value, {from: borrower}))
+            .then(() => debts.repay(borrower, value, {from: alien}))
+            .then((result) => {
+              it('equal amount and value', function(){ 
+                  getAndCompare(debts.debts, borrower, value);
+                });
+                assert.equal(result.logs.length, 0);
+            })
+    });
 
-  it('should direct you for inventing more tests');
+    async function getAndCompare(map, _address, value) {
+      var amount = await map(_address);                     
+      assert.equal(amount, value);      
+    }
+
+  // it('should direct you for inventing more tests');
 });
